2e55dd1b15dcdcf9bcc60bb5d8b843c44695bff7
XiaoShangshang <hujunhui19@dnui.edu.cn>
2021-03-10 10:30:28 +0800

    注销用户
    退出

---
+++ src/controller/LoginOutServlet.java
@@ -0,0 +1,15 @@
+package controller;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class LoginOutServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        session.removeAttribute("uid");
+        request.getRequestDispatcher("/HomePage.jsp").forward(request,response);
+    }
+
+}
